3d77f489f56d1eef4c4951fc4bf11b25d052c1d4
Egor Leskov <leskov_0_0@mail.ru>
2024-10-05 11:08:34 +0300

    added DragAndDrop and GitHubHover

---
+++ src/test/java/DragAndDrop.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDrop {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void SelenideHoverSearch() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDrop(to($("#column-b")));
+        $("#column-a").$(byText("B"));
+    }
+}
---
+++ src/test/java/GitHubHover.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GitHubHover {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void SelenideHoverSearch() {
+        open("https://github.com/");
+        $(".header-logged-out").$(byText("Solutions")).hover();
+        $("[href='https://github.com/enterprise']").click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform."));
+    }
+}
